fb7e3466129f803bab1a0d6c3e23f2b4acb2b508
Tran Ha <haqt2003@gmail.com>
2024-07-08 13:57:44 +0700

    init

---
+++ app.js
@@ -0,0 +1,56 @@
+// require("dotenv").config();
+
+const express = require("express");
+const logger = require("morgan");
+// const secureApp = require("helmet");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+
+// setup connect mongodb by mongoose
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/nodejsapistarter")
+//   .then(() => console.log("Connected"))
+//   .catch((error) => console.error(`Fail ${error}`));
+
+const app = express();
+// app.use(secureApp());
+
+// const deckRoute = require("./routes/deck");
+// const userRoute = require("./routes/user");
+
+// Middlewares
+// app.use(cors());
+app.use(logger("dev"));
+// app.use(bodyParser.json());
+
+// app.use("/decks", deckRoute);
+// app.use("/users", userRoute);
+
+// Routes
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Server is OK!",
+  });
+});
+
+// Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler function
+app.use((err, req, res, next) => {
+  const error = app.get("env") === "development" ? err : {};
+  const status = err.status || 500;
+  return res.status(status).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+const port = app.get("port") || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
